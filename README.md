0989018c0af3e228f401f6d1e4f2284c62067bb8
Vitor iqueira <vitorccsiqueira@gmail.com>
2021-06-08 16:13:19 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+yourls-api-save-by-keyword
+===================
+
+Adds a new action `save_by_keyword` to the [YOURLS API](https://yourls.org/#API).
+
+This action either updates the long URL of an existing keyword, or creates a new short URL with the supplied keyword. 
+
+This plugin is useful when you prefer to determine your own keywords (e.g: `user-id-1`, `user-id-2`, `user-joe` etc) rather than letting YOURLS generating random ones (e.g: `a93al`, `n9sj8`, etc) and you need to keep their long URLs always up to date.
+
+With this action, you may save up to three steps: check if an existing keyword already exists, then delete it, and then generating again under the same keyword.
+
+The request parameters are:
+```
+username <api_username>
+password <api_password>
+action: "save_by_keyword"
+format: "json"
+url: <required: the long URL to shorten>
+keyword: <required: the keyword>
+title: <optional: the shorten URL title>
+```
+
+**TIP:** In case you need to allow multiple short URLs for a same long URL, you need to set the parameter `YOURLS_UNIQUE_URLS` to `false`.
+```php
+# config.php
+define( 'YOURLS_UNIQUE_URLS', false );
+```
+
+# How to
+* In `/user/plugins`, create a new folder named `yourls-api-save-by-keyword`
+* Drop these files in that directory
+* Go to the Plugins administration page and activate the plugin
+* Have fun
---
+++ plugin.php
@@ -0,0 +1,92 @@
+<?php
+
+/*
+ Plugin Name: Save by Keyword
+ Plugin URI: https://github.com/vitorccs/yourls-api-save-by-keyword
+ Description: Define a custom API action 'save_by_keyword' which either creates a new short URL or update an existing one by a giving keyword
+ Version: 1.0.0
+ Author: Vitor Siqueira
+ Author URI: https://github.com/vitorccs
+ */
+
+yourls_add_filter( 'api_action_save_by_keyword', 'vccs_api_save_by_keyword' );
+
+function vccs_api_save_by_keyword() {
+    $keyword = isset( $_REQUEST['keyword'] )
+        ? $_REQUEST['keyword']
+        : '';
+
+    $url = isset( $_REQUEST['url'] )
+        ? $_REQUEST['url']
+        : '';
+
+    $title = isset( $_REQUEST['title'] )
+        ? $_REQUEST['title']
+        : '';
+
+    if ( empty( $keyword ) ) {
+        return array(
+            'statusCode' => 400,
+            'status'     => 'fail',
+            'simple'     => 'error: missing "keyword" param',
+            'message'    => 'error: missing "keyword" param',
+        );
+    }
+
+    if ( empty( $url ) ) {
+        return array(
+            'statusCode' => 400,
+            'status'     => 'fail',
+            'simple'     => 'error: missing "url" param',
+            'message'    => 'error: missing "url" param',
+        );
+    }
+
+    $keyword = yourls_get_protocol( $keyword )
+        ? yourls_get_relative_url( $keyword )
+        : $keyword;
+
+    // Prevent fail status for zero rows affected
+    if ( vccs_no_parameter_changes( $url, $keyword, $title ) ) {
+        $response = [
+            'statusCode' => 200,
+            'status'     => 'success',
+            'message'    => yourls__( 'The keyword already exists with same title and long URL' )
+        ];
+    } else {
+        $response = yourls_keyword_is_free( $keyword )
+            ? yourls_add_new_link( $url, $keyword, $title )
+            : yourls_edit_link( $url, $keyword, $keyword, $title );
+
+        // normalize response fields
+        $response = array_intersect_key( $response, [ 'statusCode' => '', 'status' => '', 'message' => '' ] );
+
+        // supply if missing field
+        if (! isset( $response['statusCode'] ) ) {
+            $response['statusCode'] = $response['status'] === 'success' ? 200 : 400;
+        }
+    }
+
+    $response['keyword'] = $keyword;
+    $response['shorturl'] = yourls_get_yourls_site().'/'.$keyword;
+
+    krsort($response);
+
+    return $response;
+}
+
+function vccs_no_parameter_changes( $url, $keyword, $title ) {
+    global $ydb;
+
+    $table = YOURLS_DB_TABLE_URL;
+    $url = yourls_sanitize_url( $url );
+    $keyword = yourls_sanitize_string( $keyword );
+    $title = yourls_sanitize_title( $title );
+
+    $bindings = array('url' => $url, 'keyword' => $keyword, 'title' => $title);
+    $sql = "SELECT COUNT(keyword) FROM `$table` WHERE `url` = :url AND `title` = :title AND `keyword` = :keyword;";
+    $count = intval($ydb->fetchValue($sql, $bindings ));
+
+    return $count > 0;
+}
+?>
